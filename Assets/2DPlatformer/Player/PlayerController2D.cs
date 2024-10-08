0f8f958393c5bc3a27fbd05a07adc2a8e8e5ff1b
Daniel-Laptop <danielnoam1999@gmail.com>
2024-10-08 20:23:46 +0300

    Moreee

--- Assets/2DPlatformer/Player/PlayerController2D.cs
+++ Assets/2DPlatformer/Player/PlayerController2D.cs
@@ -17,33 +17,82 @@ public class PlayerController2D : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int maxJumps = 2;
+    private bool isGrounded;
+    private int remainingJumps;
 
     [Header("Gravity Settings")]
     [SerializeField] private float gravityForce = 9.8f;
     [SerializeField] private float fallMultiplier = 2.5f;
 
+
+    // Input vars
+    private float horizontalInput;
+    private bool jumpInput;
+
+
+    
+
+
     
 
 
 
-    void Awake() {
+    private void Awake() {
 
         // if (!rigidBody) {rigidBody = GetComponent<Rigidbody2D>();}
         // if (!spriteRenderer) {spriteRenderer = GetComponent<SpriteRenderer>();}
         // if (!collBody) {collBody = GetComponent<Collider2D>();}
         // if (!collFeet) {collFeet = GetComponent<Collider2D>();}
-
     }
  
 
-    void Start()
+    private void Start()
     {
         
     }
 
 
-    void Update()
+    private void Update()
     {
+        CheckForInput();
+    }
+
+    private void FixedUpdate() {
+
+        CollisionChecks();
+        HandleGravity();
+        HandleMovement();
         
     }
+    
+
+    private void CheckForInput() {
+
+        // Check for horizontal movement
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Check for jump input
+        jumpInput = Input.GetButtonDown("Jump");
+    }
+
+    private void HandleMovement() {
+
+        rigidBody.velocity = new Vector2(horizontalInput * moveSpeed, rigidBody.velocity.y);
+    }
+
+    private void HandleGravity() {
+
+        // the player is not on the ground and not moving upwards
+        if ( !isGrounded && rigidBody.velocity.y < 0) { 
+            rigidBody.velocity += Vector2.up * gravityForce * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void CollisionChecks() {
+
+        // Check if the player is grounded
+        isGrounded = collFeet.IsTouchingLayers(groundLayer);
+    }
+    
 }
